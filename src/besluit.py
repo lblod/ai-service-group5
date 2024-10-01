af480cef2aeb12cd76f4511d3757680531c11c71
Tim Standaert <tim.standaert@inuits.eu>
2024-10-01 09:49:32 +0000

    adding test besluit

---
+++ src/besluit.py
@@ -0,0 +1,60 @@
+besluit_plaintext = """
+Retributiereglement voor het gebruik van repetitielokaal The Basement Lievegem
+
+Artikel 1    Doel
+
+Dit retributiereglement heeft tot doel retributies vast te leggen die zijn verschuldigd door de gebruikers van de repetitieruimte te Hofbouwstraat 1, 9930 Lievegem
+
+Artikel 2    Tarieven
+
+Voor het gebruik van de repetitieruimte “The Basement” in ’t Kwadrant, Hofbouwstraat 1, 9930 Lievegem worden volgende retributies aangerekend:
+
+Individuele beoefenaars    2 euro per uur (Maximum 4 uur aaneensluitend)
+Groepen:    4 euro per uur (Maximum 4 uur aaneensluitend)
+Dagtarief (Na afspraak met Team Evenementen)    19 euro
+Jaartarief    190 euro per jaar voor groepen en individuele beoefenaars, jaarlijks te betalen.
+
+Deze tarieven omvatten naast het gebruik van de zaal ook de aanwezige basisuitrusting en de energiekosten.
+
+Artikel 3    Betalingsmodaliteiten
+
+§1. De retributie is te betalen binnen de dertig dagen na verzending van de factuur.
+
+§2. Bij gebrek aan betaling zal de invordering van onbetwiste en opeisbare niet-fiscale ontvangsten gebeuren conform de bepalingen van artikel 177, tweede lid, van het decreet lokaal bestuur van 22 december 20217. Betwiste, opeisbare niet-fiscale ontvangsten worden bij niet-betaling burgerrechtelijk ingevorderd.
+
+§3. Betwistingen van de factuur moeten gebeuren binnen de dertig dagen volgend op de verzending van de factuur ter attentie van het college van burgemeester en schepenen.
+
+§4. Bij gebrek aan betaling vervallen alle reeds vastgelegde reservaties en worden bovendien geen nieuwe reservaties aanvaard tot op het ogenblik dat de verschuldigde som werd betaald.
+
+Artikel 4    Oneigenlijk gebruik van de ruimte
+
+§1. De gebruiker verklaart dat de gegevens op het aanvraagformulier conform zijn met betrekking tot de aanvrager en de geplande activiteit.
+
+§2. De gebruiker kan de accommodatie niet ter beschikking stellen van derden.
+
+§3. Indien zou blijken dat de werkelijke activiteit niet overeenstemt met de toegelaten activiteit wordt een schadevergoeding opgelegd van 125,00 euro. Voorts kan aan de betrokkene het verder gebruik van de accommodatie ontzegd worden. Uitspraak hieromtrent gebeurt door het college van burgemeester en schepenen op advies van het team evenementen.
+
+Bij alle inbreuken en/of beschadigingen worden de materiaalkost en/of de ingezette personeelsuren om alles in een correcte staat te herstellen door de gebruiker vereffend volgens de hem voorgelegde factuur, goedgekeurd door het college van burgemeester en schepenen.
+
+Artikel 5    Modaliteiten
+
+Alle modaliteiten rond het gebruik van de repetitieruimte worden beschreven in het gebruikersreglement.
+
+Bekendmaking
+
+Beheersorgaan gemeentelijke infrastructuur en team financiën, team evenementen.
+Publieke stemming
+Aanwezig: Nicholas Spinel, Kim Martens, Caroline Fredrick, Chris De Wispelaere, Guy De Neve, Jeroen Van Acker, Martine Lataire-Gyssels, Vincent Laroy, Hilde De Graeve, Ivan Goethals, Steven Lambert, Jurgen Blomme, Dirk De Poorter, Tim Maenhout, Judith De Muynck, Nik Braeckman, Ann Boterdaele, Didier Garré, Patrick Dossche, Wouter De Muynck, Marc Boterdaele, Lut Van der Spurt, Gunter Lippens, Wouter Ryckaert, Christophe Huysman, Eddy De Mits
+Voorstanders: Nicholas Spinel, Kim Martens, Caroline Fredrick, Chris De Wispelaere, Guy De Neve, Jeroen Van Acker, Martine Lataire-Gyssels, Vincent Laroy, Hilde De Graeve, Ivan Goethals, Steven Lambert, Jurgen Blomme, Dirk De Poorter, Tim Maenhout, Judith De Muynck, Nik Braeckman, Ann Boterdaele, Didier Garré, Patrick Dossche, Wouter De Muynck, Marc Boterdaele, Lut Van der Spurt, Gunter Lippens, Wouter Ryckaert, Christophe Huysman
+Resultaat: Goedgekeurd met eenparigheid van stemmen.
+Besluit
+
+Artikel 1
+De gemeenteraad stelt het aangepast retributiereglement voor het gebruik van repetitielokaal The Basement Lievegem vast.
+
+Artikel 2
+Het aangepast retributiereglement treedt in werking vanaf 1 juni 2024.
+
+Artikel 3
+Het retributiereglement voor het gebruik van repetitielokaal The Basement Lievegem, vastgesteld op de gemeenteraad van 18 december 2019 wordt opgeheven.
+"""
