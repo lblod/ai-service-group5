6508786d23a4741944ecb64a077f5b622047aea3
Tim Standaert <tim.standaert@inuits.eu>
2024-10-01 08:40:20 +0000

    some moving around

--- src/web.py
+++ src/web.py
@@ -1,3 +1,13 @@
+#import ollama
+
+@app.route("/extract-voorwaarden")
+def extract(dummy):
+    return 'some value'
+
 @app.route("/hello")
 def hello():
-    return "Hello from the mu-python-template!"
+    return "Hello from some template!"
+
+@app.route("/bye")
+def bye():
+    return "goodbye"
