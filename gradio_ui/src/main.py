46d9710f950ccd4b7b39b9f111d478451c3285f6
Tim Standaert <tim.standaert@inuits.eu>
2024-10-02 12:18:20 +0000

    small change to ui

--- gradio_ui/src/main.py
+++ gradio_ui/src/main.py
@@ -6,7 +6,7 @@ from openai import OpenAI
 import mlflow
 
 mlflow.set_tracking_uri("http://mlflow:9292")
-mlflow.openai.autolog(log_models=True, log_model_signatures=True)
+mlflow.openai.autolog(log_models=True, log_model_signatures=True, log_traces=True, registered_model_name='llama3.1')
 mlflow.set_experiment('abb-hackathon')
 mlflow.start_run()
 
@@ -37,12 +37,9 @@ demo = gr.Interface(
         outputs=['text'],
         )
 
-def complete(system_prompt, prompt, text):
+def complete(system_prompt, text):
 
-    comb_prompt = f'''{prompt}
-
-    ------------------------
-    Document: {text}
+    comb_prompt = f'''{text}
     '''
 
     response = OAIClient.chat.completions.create(model="llama3.1", messages=[
@@ -68,14 +65,13 @@ def complete(system_prompt, prompt, text):
 with gr.Blocks() as testblock:
 
     system_prompt = gr.Textbox(label="system prompt", show_label=True)
-    user_prompt = gr.Textbox(label="user prompt", show_label=True)
     besluit = gr.Textbox(label="besluit", show_label=True)
 
     output = gr.Textbox(label="output", show_label=True)
 
     submit_button = gr.Button(value='submit')
     
-    submit_button.click(complete, inputs=[system_prompt, user_prompt, besluit], outputs = [output])
+    submit_button.click(complete, inputs=[system_prompt, besluit], outputs = [output])
 
 #test = gr.Interface( fn = complete, inputs=['text', 'text'], outputs=['text'])
 
